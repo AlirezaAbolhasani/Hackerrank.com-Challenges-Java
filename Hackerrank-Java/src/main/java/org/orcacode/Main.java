ebd64a3804c01a233640867c8bbe93bf53899120
AlirezaAbolhasani <alireza.ab.1111.com>
2025-11-26 12:05:25 +0330

    14040905-Start

---
+++ Hackerrank-Java/src/main/java/org/orcacode/Main.java
@@ -0,0 +1,12 @@
+package org.orcacode;
+
+/**
+ * Alireza Abolhasani
+ * Default (Template) Project
+ * 11/26/2025
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
